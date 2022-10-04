5caf9e8da636a3c26669929004983be5bf8c18d8
mao <c0b2107196@edu.teu.ac.jp>
2022-10-04 17:15:53 +0900

    練習問題5まで

---
+++ ex02/pp5.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+def click_number(event): # 練習3
+    btn = event.widget
+    num = int(btn["text"])
+    entry.insert(tk.END, num) # 練習5
+
+
+root = tk.Tk() # 練習1
+root.geometry("300x500")
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry.grid(row=0, column=0, columnspan=3)
+r, c = 1, 0 # r: 行を表す変数／c：列を表す変数
+for i, num in enumerate(range(9,-1, -1), 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn.bind("<1>", click_number)
+    btn.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+root.mainloop()

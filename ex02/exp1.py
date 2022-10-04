5a36cf1560f744ec5f19c6de1e8716faffa819e7
mao <c0b2107196@edu.teu.ac.jp>
2022-10-04 16:52:06 +0900

    演習２

---
+++ ex02/exp1.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def click_number(event):
+    btn = event.widget
+    num = btn["text"]
+    entry.insert(tk.END, num)
+
+
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
+
+def click_clear(event):
+    entry.delete(0, tk.END)
+
+
+root = tk.Tk()
+root.geometry("300x600")
+
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right")
+entry.grid(row=0, column=0, columnspan=3)
+
+r, c = 1, 0
+numbers = list(range(9, -1, -1))
+operators = ["+", "-", "*", "/", "(",")"]
+for i, num in enumerate(numbers+operators, 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=3, height=1)
+    btn.bind("<1>", click_number)
+    btn.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+
+btn = tk.Button(root, text=f"=", font=("", 30), width=3, height=1)
+btn.bind("<1>", click_equal)
+btn.grid(row=r, column=c)
+
+btn = tk.Button(root, text=f"C", font=("", 30), width=3, height=1)
+btn.bind("<1>", click_clear)
+btn.grid(row=r, column=c+1)
+
+root.mainloop()
